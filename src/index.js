0f7fe7655094d31238983efc85ce0ff3e39b09b2
Daniel Gomes de Lima <50140741+dnlgomesl@users.noreply.github.com>
2023-03-29 16:57:41 -0300

    remove mock do index

--- src/index.js
+++ src/index.js
@@ -2,31 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App'
-import BarChart from './app/components/BarChart/BarChart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const data = {
-  "neo classical metal": 1,
-  "progressive metal": 1,
-  "adoracao": 3,
-  "brazilian gospel": 1,
-  "rock gospel brasileiro": 1,
-  "neo mellow": 1,
-  "singer-songwriter": 1,
-  "brazilian hip hop": 2,
-  "brazilian emo": 1,
-  "brazilian rock": 1,
-  "pop rock brasileiro": 1,
-  "rock alternativo brasileiro": 1,
-  "indie cristao": 2,
-  "alternative metal": 1,
-  "nu metal": 1,
-  "rap metal": 1,
-  "funk carioca": 1
-}
 root.render(
   <React.StrictMode>
     <App />
-    <BarChart data={data} />
   </React.StrictMode>
 );
