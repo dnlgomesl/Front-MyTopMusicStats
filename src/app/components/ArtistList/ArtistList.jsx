1aca0233b76952813cde9da574da6d78f8fdae70
Daniel Gomes de Lima <50140741+dnlgomesl@users.noreply.github.com>
2023-04-13 11:16:40 -0300

    adicionando logo do spotify

--- src/app/components/ArtistList/ArtistList.jsx
+++ src/app/components/ArtistList/ArtistList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ArtistList.css'; // importando arquivo de estilos
+import logos from '../../static/Spotify_Logo_CMYK_White.png'
 
 function ArtistList({artists}) {
   return (
@@ -9,14 +10,17 @@ function ArtistList({artists}) {
           <h2>{idx+1}</h2>
           <div className='container-infos'>
             <div className='infos'>
+              <div className='url-img'>
+                <img src={logos} alt="logo-spotify" />
+              </div>
               <p>Artista: {artist.artist}</p>
               <p>Gêneros: {artist.genres.join(", ")}</p>
               <p>Popularidade: {artist.popularity}</p>
-              <div className='url'>
+              <div className='url-img'>
                 <a href={artist.url}>Link to Spotify</a>
               </div>
             </div>
-            <img src={artist.images[1].url} alt='track' />
+            <img src={artist.images[1].url} alt='track'/>
           </div>
         </li>
       ))}
--- src/app/components/TrackList/TrackList.jsx
+++ src/app/components/TrackList/TrackList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './TrackList.css'; // importando arquivo de estilos
+import logos from '../../static/Spotify_Logo_CMYK_White.png'
+
 
 function TrackList({tracks}) {
   return (
@@ -9,12 +11,15 @@ function TrackList({tracks}) {
           <h2>{idx+1}</h2>
           <div className='container-infos'>
             <div className='infos'>
+            <div className='url-img'>
+                <img src={logos} alt="logo-spotify" />
+              </div>
               <p>Música: {track.music}</p>
               <p>Artista: {track.artist}</p>
               <p>Álbum: {track.album}</p>
               <p>Duração: {track.duration}</p>
               <p>Popularidade: {track.popularity}</p>
-              <div className='url'>
+              <div className='url-img'>
                 <a href={track.url}>Link to Spotify</a>
               </div>
             </div>
