f64076f67fb7fd64efceba41a22559c610432947
Daniel Gomes de Lima <50140741+dnlgomesl@users.noreply.github.com>
2023-10-09 08:36:00 -0300

    Atualizar o Login.jsx

--- src/app/components/Login/Login.jsx
+++ src/app/components/Login/Login.jsx
@@ -3,7 +3,7 @@ import TokenContext from "../../contexts/Token";
 import "./Login.css";
 
 function Login() {
-    const CLIENT_ID='d883dc40f4d840efa20f6b4b21eb3cd1'
+    const CLIENT_ID=''
     const REDIRECT_URI='https://my-top-music-stats.vercel.app/'
     const AUTH_ENDPOINT='https://accounts.spotify.com/authorize'
     const RESPONSE_TYPE='token'
